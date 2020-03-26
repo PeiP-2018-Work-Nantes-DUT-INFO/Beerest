b2b12d4e3b707c6b68c6e6e85f18c8da37aac98a
adamenaji <adamenaji@hotmail.fr>
2020-03-26 09:37:06 +0100

    Create breweryRoutes.js

---
+++ app/routes/api/breweryRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+
+const { checkSchema } = require('express-validator')
+
+const BreweryController = require('../../controller/breweryController')
+const breweryController = new BreweryController()
+
+const searchBrewery = require('./schemas/SearchBeer')
+
+router.get('/', function (req, res) {
+    breweryController.findAll(res)
+})
+
+router.post('/', function (req, res) {
+    breweryController.create(req, res)
+})
+/*router.get('/search', checkSchema(searchBeer), function (req, res) {
+    breweryController.search(req, res)
+})*/
+router.get('/:id', function (req, res) {
+    breweryController.findById(req, res)
+})
+
+router.delete('/:id', function (req, res) {
+    breweryController.deleteById(req)
+})
+
+router.put('/:id', function (req, res) {
+    breweryController.update(req, res)
+})
+
+module.exports = router
