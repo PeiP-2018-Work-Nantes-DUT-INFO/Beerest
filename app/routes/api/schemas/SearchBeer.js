922ae0a4c31143cde011ee2ac6b8d32aac18740e
Eliott Dubois <eliott.dubois@gmail.com>
2020-03-26 10:23:59 +0100

    Fix search for beer

--- app/routes/api/schemas/SearchBeer.js
+++ app/routes/api/schemas/SearchBeer.js
@@ -1,106 +1,106 @@
 module.exports = {
-    city: {
-      in: ['query'],
-      optional: { 
-        options: { nullable: true }
-      },
-      isAlphanumeric: true
+  city: {
+    in: ['query'],
+    optional: {
+      options: { nullable: true }
     },
-    country: {
-      in: ['query'],
-      optional: { 
-        options: { nullable: true }
-      },
-      isAlphanumeric: true
+    isAlphanumeric: true
+  },
+  country: {
+    in: ['query'],
+    optional: {
+      options: { nullable: true }
     },
-    degAbove: {
-      in: ['query'],
-      optional: { 
-        options: { nullable: true }
-      },
-      isFloat: {
-        options: {
-          isFloat: true,
-          min: 0,
-          max: 100
-        }
-      },
-      toFloat: true
+    isAlphanumeric: true
+  },
+  degAbove: {
+    in: ['query'],
+    optional: {
+      options: { nullable: true }
     },
-    degBelow: {
-      in: ['query'],
-      optional: { 
-        options: { nullable: true }
-      },
-      isFloat: {
-        options: {
-          isFloat: true,
-          min: 0,
-          max: 100
-        }
-      },
-      toFloat: true
-    },
-    limit: {
-      in: ['query'],
-      optional: { 
-        options: { nullable: true }
-      },
-      isInt: {
-        options: {
-          isInt: true,
-          min: 1
-        }
-      },
-      toInt: true
-    },
-    orderBy: {
-      in: ['query'],
-      optional: { 
-        options: { nullable: true }
-      },
-      isIn: {
-        options: [[
-          'name',
-          'id',
-          'brewery_id',
-          'cat_id',
-          'style_id',
-          'alcohol_by_volume',
-          'international_bitterness_units ',
-          'standard_reference_method',
-          'universal_product_code',
-          'filepath',
-          'description',
-          'add_user',
-          'last_mod',
-          'style',
-          'category',
-          'brewer',
-          'address',
-          'city',
-          'state',
-          'country',
-          'coordinates',
-          'website',
-        ]]
+    isFloat: {
+      options: {
+        isFloat: true,
+        min: 0,
+        max: 100
       }
     },
-    page: {
-      in: ['query'],
-      optional: { 
-        options: { nullable: true }
-      },
-      isInt: {
-        options: {
-          isInt: true,
-          min: 0
-        }
-      },
-      toInt: true,
-      custom: {
-        errorMessage: 'Limit parameter is required',
-        options: (value, { req }) => !!req.query.limit
-      },
+    toFloat: true
+  },
+  degBelow: {
+    in: ['query'],
+    optional: {
+      options: { nullable: true }
+    },
+    isFloat: {
+      options: {
+        isFloat: true,
+        min: 0,
+        max: 100
+      }
+    },
+    toFloat: true
+  },
+  limit: {
+    in: ['query'],
+    optional: {
+      options: { nullable: true }
+    },
+    isInt: {
+      options: {
+        isInt: true,
+        min: 1
+      }
+    },
+    toInt: true
+  },
+  orderBy: {
+    in: ['query'],
+    optional: {
+      options: { nullable: true }
+    },
+    isIn: {
+      options: [[
+        'name',
+        'id',
+        'brewery_id',
+        'cat_id',
+        'style_id',
+        'alcohol_by_volume',
+        'international_bitterness_units ',
+        'standard_reference_method',
+        'universal_product_code',
+        'filepath',
+        'description',
+        'add_user',
+        'last_mod',
+        'style',
+        'category',
+        'brewer',
+        'address',
+        'city',
+        'state',
+        'country',
+        'coordinates',
+        'website'
+      ]]
+    }
+  },
+  page: {
+    in: ['query'],
+    optional: {
+      options: { nullable: true }
+    },
+    isInt: {
+      options: {
+        isInt: true,
+        min: 0
+      }
+    },
+    toInt: true,
+    custom: {
+      errorMessage: 'Limit parameter is required',
+      options: (value, { req }) => !!req.query.limit
     }
   }
+}
